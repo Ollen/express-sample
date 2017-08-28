092a41ec14077ddbf462dfda34cee647dd9d1b22
Allendale Nato <natoallendale@gmail.com>
2017-08-28 12:25:07 +0800

    Add static files

--- server.js
+++ server.js
@@ -3,9 +3,16 @@
  */
 
 const express = require('express');
+const hbs = require('hbs');
+
 const port = process.env.PORT || 3000;
 
 let server = express();
+
+server.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials')
+
+server.use('/scripts', express.static(__dirname + '/bower_components'));
 
 server.listen(port, () => {
     console.log(`Server started on port ${port}`);
