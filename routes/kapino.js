ab79e93457722135bda70c64b6ddf4cd0e308186
Allendale Nato <natoallendale@gmail.com>
2017-08-28 18:28:04 +0800

    Add about and browse page

--- routes/kapino.js
+++ routes/kapino.js
@@ -5,4 +5,12 @@ router.get('/', (req, res) => {
     res.render('home.hbs');
 });
 
+router.get('/browse', (req, res) => {
+    res.render('browse.hbs');
+});
+
+router.get('/about', (req, res) => {
+    res.render('about.hbs');
+});
+
 module.exports = router;
